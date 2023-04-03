9cff2afc9660790b1ea73b40b2e582bf080f7ce7
Stephane De Andrade <stephdeandrade21@gmail.com>
2023-04-03 18:38:47 +0200

    Header-menu

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.header').removeClass('nav-down').addClass('nav-up');
+        $('.nav-button').removeClass('hidden').addClass('show');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.header').removeClass('nav-up').addClass('nav-down');
+            $('.nav-button').removeClass('show').addClass('hidden');
+
+        }
+    }
+    
+    lastScrollTop = st;
+}   
+
+const menu = document.querySelector(".nav-menu")
+const navButton = document.querySelector(".nav-button")
+const menuButton = document.querySelector(".menu-btn")
+const navBtn = document.querySelector(".nav-btn")
+
+navButton.addEventListener('click', () => {
+    menu.classList.toggle("active-r")
+    menuButton.classList.toggle("active")
+    navBtn.classList.toggle("close")
+    document.body.classList.toggle("of")
+})
