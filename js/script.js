6d1960c720de9f54ec7303bdfb95280a4af3f07f
Esadd <jonathan.un@icloud.com>
2023-04-05 10:08:49 +0200

    Horizontal scroll fini

--- js/script.js
+++ js/script.js
@@ -35,13 +35,13 @@ const section_2 = document.getElementById("horizontal")
 let box_items = gsap.utils.toArray(".horizontal__item");
 
 gsap.to(box_items, {
-    xPercent: -100 * (box_items.length - 1),
+    xPercent: -120 * (box_items.length - 1),
     ease: "sine.out",
     scrollTrigger: {
         trigger: section_2,
         pin: true,
         scrub: 3,
-        snap: 1 / (box_items.length - 1),
+        snap: 2 / (box_items.length - 1),
         end: "+=" + section_2.offsetWidth
     }
 });
